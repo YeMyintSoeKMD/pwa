de8b8043f22b3c17f2b5b93517d1bba223daccd9
Ye Myint Soe <64119062+YeMyintSoeKMD@users.noreply.github.com>
2024-08-26 00:05:13 +0630

    Create sw2.js

---
+++ sw2.js
@@ -0,0 +1,44 @@
+const cacheName = 'v1'
+
+// Install service worker
+self.addEventListener('install', e => {
+  console.log('SW installed')
+})
+
+// Activate service worker 
+self.addEventListener('activate', e => {
+    /* Delete old caches */
+    e.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if(cache !== cacheName) {
+                        console.log('SW: Deleted old caches');
+                        return caches.delete(cache)
+                    }
+                })
+            )
+        })
+    )
+})
+
+// Fetch event
+// Fetch event
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        fetch(e.request)
+        .then(res => {
+            // make copy clone of response
+            const resClone = res.clone()
+            // Open cache
+            caches
+            .open(cacheName)
+            .then(cache => {
+                // add response to the cache
+                cache.put(e.request, resClone)
+            })
+            return res
+        })
+        .catch(() => caches.match(e.request).then(res => res))
+    )
+})
