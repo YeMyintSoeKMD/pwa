7336280b1282c4585f373342f50471d7f60242b9
Ye Myint Soe <64119062+YeMyintSoeKMD@users.noreply.github.com>
2024-08-29 00:06:27 +0630

    Update frontend-sw.js

--- push-notification/frontend-sw.js
+++ push-notification/frontend-sw.js
@@ -1,1 +1,55 @@
-sdf
+/* STEP 1 - Register SW at main.js */
+// Service worker registration
+if("serviceWorker" in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js')
+        .then(() => console.log('sw registered'))
+        .catch(err => console.log(err))
+    })
+}
+
+/* STEP 2 - Implement Push Event for Showing Notification when Notification is sent from the Server at sw.js*/
+/* Pushing */
+self.addEventListener('push', event => {
+    const notification = event.data.json();
+    event.waitUntil(
+        // {"title": "Hello customer!", "body": "New product is out now.", "url": "/products/26"}
+        self.registration.showNotification(notification.title, {
+            body: notification.body,
+            icon: '/src/assets/icons/icon-144x144.png',
+            data: {
+                notifURL: notification.url
+            }
+          }),
+    )
+})
+
+self.addEventListener('notificationclick', event => {
+    event.waitUntil(
+        clients.openWindow(event.notification.data.notifURL)
+    )
+})
+
+/* STEP 3 - Allow permission and subscribe to server where your button exists */
+const requestPermission = () => {
+    Notification.requestPermission().then((permission) => {
+        if (permission === "granted") {
+            // get service worker
+            navigator.serviceWorker.ready.then(sw => {
+                // subscribe 
+                sw.pushManager.subscribe({
+                    userVisibleOnly: true,
+                    applicationServerKey: "BCKU6syjcBdoV3EO3w-k8nior__YBYCTtBPDeD5oqGTwG5D6WVCOdGk2TCOaMQhcJK6-9MvNSAqlb_97CWXshmU"
+                }).then(subscription => {
+                    // subscription successful
+                    axios.post('/push-subscribe', subscription)
+                        .then(res => {
+                            console.log(res)
+                            alert('You will get some notification from us :)')
+                        })
+                })
+            })
+        }
+    });
+}
+
